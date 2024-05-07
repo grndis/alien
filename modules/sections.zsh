b84299869aea3ac2294ba49b023a791cfcfaf870
Grandis SYF <gra@dr.com>
2024-05-07 17:45:02 +0700

    new line for prompt

--- modules/sections.zsh
+++ modules/sections.zsh
@@ -142,7 +142,7 @@ alien_prompt_section_venv() {
 
 alien_prompt_section_prompt() {
   __section=(
-    content "%B${ALIEN_PROMPT_SYM}%b "
+    content $'\n'
     foreground $ALIEN_PROMPT_FG
   )
 }
